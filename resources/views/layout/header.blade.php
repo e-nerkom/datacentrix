14f29dbe6f5b796694b12d4fa893bed183dbe381
muhammadzakinudin <zakinugraha001@gmail.com>
2021-05-10 14:52:11 +0700

    Change color mobile template

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -58,73 +58,10 @@
                                         </div>
                                         <nav class="rs-menu pr-65">
                                             <ul class="nav-menu">
-                                                <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item"> <a href="#">Home</a>
-                                                    <ul class="mega-menu home2"> 
-                                                        <li class="mega-menu-container">
-                                                            <div class="mega-menu-innner">
-                                                                <div class="single-megamenu">
-                                                                    <ul class="sub-menu">
-                                                                        <li class="menu-title">Home Multipage</li>
-                                                                        <li><a href="index.html">Business Main</a> </li>
-                                                                        <li><a href="index2.html">Business 3</a> </li>
-                                                                        <li><a href="index3.html">Consulting 2</a> </li>
-                                                                        <li><a href="index4.html">Consulting 1</a> </li>
-                                                                        <li><a href="index5.html">Business 2</a> </li>
-                                                                        <li><a href="index6.html">Insurance</a> </li>
-                                                                        <li><a href="index7.html">Saas</a> </li>
-                                                                        <li><a href="index8.html">Human Resource</a> </li>
-                                                                        <li><a href="index9.html">Digital Agency</a> </li>
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="single-megamenu">
-                                                                    <ul class="sub-menu">
-                                                                        <li class="menu-title">Home Multipage</li>
-                                                                        <li><a href="index10.html">Law Firm</a></li>
-                                                                        <li><a href="index11.html">Consulting 3</a></li>
-                                                                        <li><a href="index12.html">Digital Agency(SEO)</a></li>
-                                                                        <li><a href="index13.html">Digital Agency</a></li>
-                                                                        <li><a href="index14.html">Corporate Business</a></li>
-                                                                        <li><a href="index15.html">App Landing</a></li>
-                                                                        <li class="active"><a href="index16.html">It Solutions</a></li>
-                                                                        <li><a href="index17.html">Marketing Agency</a></li>
-                                                                        <li class="last-item"><a href="index18.html">Corporate Business 2</a></li>
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="single-megamenu">
-                                                                 <ul class="sub-menu last-sub-menu">
-                                                                     <li class="menu-title">Home Onepage</li>
-                                                                     <li><a href="onepage1.html">Onepage 1</a> </li>
-                                                                     <li><a href="onepage2.html">Onepage 2</a> </li>
-                                                                     <li><a href="onepage3.html">Onepage 3</a> </li>
-                                                                     <li><a href="onepage4.html">Onepage 4</a> </li>
-                                                                     <li><a href="onepage5.html">Onepage 5</a> </li>
-                                                                     <li><a href="onepage6.html">Onepage 6</a> </li>                        
-                                                                     <li><a href="onepage8.html">Onepage 8</a> </li>
-                                                                     <li><a href="onepage9.html">Onepage 9</a> </li>
-                                                                     <li><a href="onepage10.html">Onepage 10</a> </li>
-                                                                 </ul>
-                                                             </div>
-                                                             <div class="single-megamenu">
-                                                                 <ul class="sub-menu last-sub-menu">
-                                                                     <li class="menu-title">Home Onepage</li>
-                                                                     <li><a href="onepage11.html">Onepage 11</a> </li>
-                                                                     <li><a href="onepage12.html">Onepage 12</a> </li>
-                                                                     <li><a href="onepage13.html">Onepage 13</a> </li>
-                                                                     <li><a href="onepage14.html">Onepage 14</a> </li>
-                                                                     <li><a href="onepage15.html">Onepage 15</a> </li>
-                                                                     <li><a href="onepage16.html">Onepage 16</a> </li>
-                                                                     <li><a href="onepage17.html">Onepage 17</a> </li>
-                                                                     <li><a href="#">Coming Soon</a> </li>
-                                                                     <li class="last-item"><a href="#">Coming Soon</a> </li>
-                                                                 </ul>
-                                                             </div>
-                                                            </div>
-                                                        </li>
-                                                    </ul> <!-- //.mega-menu --> 
-                                                </li>
+                                                <li class="rs-mega-menu mega-rs current-menu-item"> <a href="#">Home</a></li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Pages</a>
+                                                    <a href="#">Perusahaan</a>
                                                     <ul class="sub-menu">
                                                         <li><a href="about.html">About Us</a></li>
                                                         <li class="menu-item-has-children">
@@ -148,7 +85,7 @@
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Portfolios</a>
+                                                    <a href="#">Produk</a>
                                                     <ul class="sub-menu">
                                                         <li><a href="portfolio.html">Portfolio One</a></li>
                                                         <li><a href="portfolio2.html">Portfolio Two</a></li>
@@ -168,7 +105,7 @@
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Blog</a>
+                                                    <a href="#">Layanan</a>
                                                     <ul class="sub-menu">
                                                         <li><a href="blog.html">Blog</a></li>
                                                         <li class="last-item"><a href="blog-single.html">Blog Single</a></li>
@@ -176,7 +113,7 @@
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Shop</a>
+                                                    <a href="#">Solusi</a>
                                                     <ul class="sub-menu">
                                                         <li><a href="shop.html">Shop</a></li>
                                                         <li><a href="shop-single.html">Shop Single</a></li>
@@ -186,13 +123,7 @@
                                                     </ul>
                                                 </li>
 
-                                                <li class="menu-item-has-children">
-                                                    <a href="#">Contact</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="contact.html">Contact 1</a></li>
-                                                        <li class="last-item"><a href="contact2.html">Contact 2</a></li>
-                                                    </ul>
-                                                </li>
+                                                <li class=""><a href="#">Kontak</a></li>
                                             </ul> <!-- //.nav-menu -->
                                         </nav>
                                     </div> <!-- //.main-menu -->
@@ -201,19 +132,6 @@
                                             <li class="search-parent">
                                                 <a class="hidden-xs rs-search" data-target=".search-modal" data-toggle="modal" href="#">
                                                     <i class="flaticon-search"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a id="nav-expander" class="humburger nav-expander" href="#">
-                                                    <span class="dot1"></span>
-                                                    <span class="dot2"></span>
-                                                    <span class="dot3"></span>
-                                                    <span class="dot4"></span>
-                                                    <span class="dot5"></span>
-                                                    <span class="dot6"></span>
-                                                    <span class="dot7"></span>
-                                                    <span class="dot8"></span>
-                                                    <span class="dot9"></span>
                                                 </a>
                                             </li>
                                         </ul>
