27ef88439ad2097c7b379bea9399aa938bcc66a1
ahmadmiqdad5 <ahmadmiqdad696@gmail.com>
2021-06-21 15:06:42 +0700

    ganti gambar

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,10 +50,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
-<<<<<<< HEAD
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-=======
->>>>>>> 5633789bbb20dbd5e13032a43d246c1a71024275
         ],
 
     ],
