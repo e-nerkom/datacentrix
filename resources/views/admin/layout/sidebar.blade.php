1754f04c6fd6268319bb8d65f6e1da4a3234fa0c
muhammadzakinudin <zakinugraha001@gmail.com>
2021-06-21 14:59:48 +0700

    Create templating admin

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,57 @@
+<!-- Sidebar Start -->
+<aside class="sidebar" data-trigger="scrollbar">
+            <!-- Sidebar Profile Start -->
+            <div class="sidebar--profile">
+                <div class="profile--img">
+                    <a href="profile.html">
+                        <img src="/admin/assets/img/avatars/01_80x80.png" alt="" class="rounded-circle">
+                    </a>
+                </div>
+
+                <div class="profile--name">
+                    <a href="profile.html" class="btn-link">Henry Foster</a>
+                </div>
+
+                
+            </div>
+            <!-- Sidebar Profile End -->
+
+            <!-- Sidebar Navigation Start -->
+            <div class="sidebar--nav">
+                <ul>
+                    <li>
+                        <ul>
+                            <li>
+                                <a href="/admin/welcome">
+                                    <i class="fa fa-home"></i>
+                                    <span>Dashboard</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/admin/#">
+                                    <i class="fa fa-home"></i>
+                                    <span>Produk</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/admin/#">
+                                    <i class="fa fa-home"></i>
+                                    <span>Anggota Tim</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/admin/#">
+                                    <i class="fa fa-home"></i>
+                                    <span>Tentang Perusahaan</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- Sidebar Navigation End -->
+
+            
+        </aside>
+        <!-- Sidebar End -->
