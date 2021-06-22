a088078af2f00c2497137e3453703d2a7e7fcd3e
muhammadzakinudin <zakinugraha001@gmail.com>
2021-06-22 13:16:32 +0700

    Fix contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@
     <div class="rs-breadcrumbs bg-3">
         <div class="container">
             <div class="content-part text-center">
-                <h1 class="breadcrumbs-title white-color mb-0">About</h1>
+                <h1 class="breadcrumbs-title white-color mb-0">Contact</h1>
             </div>
         </div>
     </div>
@@ -22,14 +22,14 @@
                     <div class="col-lg-8 form-part white-bg">
                         <div class="sec-title mb-45">
                             <!-- <div class="sub-title primary">HUBUNGI KAMI</div> -->
-                            <h2 class="title mb-0">Hubungi Kami</h2>
+                            <h2 class="title mb-0">Contact Us</h2>
                         </div>
                         <div id="form-messages"></div>
                         <form id="contact-form" class="contact-form" method="post" action="mailer.php">
                             <div class="row">
                                 <div class="col-md-6 mb-30">
                                     <div class="common-control form-group mb-0">
-                                        <input type="text" name="name" placeholder="Nama" required="">
+                                        <input type="text" name="name" placeholder="Name" required="">
                                     </div>
                                 </div>
                                 <div class="col-md-6 mb-30">
@@ -62,14 +62,16 @@
                     </div>
                     <div class="col-lg-4 pl-0 md-pr-0 md-order-first">
                         <div class="contact-info">
-                            <h3 class="title">Kantor Pusat</h3>
+                            <h3 class="title">Office</h3>
                             <div class="info-wrap mb-20">
                                 <div class="icon-part">
                                     <i class="flaticon-location"></i>
                                 </div>
                                 <div class="content-part">
                                     <h4>Main Office</h4>
-                                    127 Double Street, Dublin, United Kingdom.
+                                    Palma Tower, 20th Floor<br>
+                                    Jl. R.A Kartini III-S Kav. 06<br>
+                                    Jakarta Selatan 12310
                                 </div>
                             </div>
                             <div class="info-wrap mb-20">
@@ -77,9 +79,8 @@
                                     <i class="flaticon-call"></i>
                                 </div>
                                 <div class="content-part">
-                                    <h4>Telepon</h4>
-                                    <p>P: <a href="tel:+1235558888">(+123) 555 8888</a></p>
-                                    <p>P: <a href="tel:+1235558899">(+123) 555 8899</a></p>
+                                    <h4>Phone</h4>
+                                    <p>P: <a href="tel:02122580028">(021) 225 80028</a></p>
                                 </div>
                             </div>
                             <div class="info-wrap mb-20">
@@ -88,8 +89,7 @@
                                 </div>
                                 <div class="content-part">
                                     <h4>Email</h4>
-                                    <p>E: <a href="mailto:support@rstheme.com">support@rstheme.com</a></p>
-                                    <p>E: <a href="mailto:info@codesless.com">info@codesless.com</a></p>
+                                    <p>E: <a href="mailto:info@datacentrix.co.id">info@datacentrix.co.id</a></p>
                                 </div>
                             </div>
                             <!-- <div class="info-wrap">
