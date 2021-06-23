63fa365dae80c2c79e9a009b9b74d0313c5117a5
muhammadzakinudin <zakinugraha001@gmail.com>
2021-06-23 13:17:44 +0700

    Fixing product id url

--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -2,52 +2,53 @@
 @section('content')
 
 <style>
+    nav>.nav.nav-tabs {
+        border: none;
+        color: #fff;
+        background: #272e38;
+        border-radius: 0;
+    }
 
-nav > .nav.nav-tabs{
+    nav>div a.nav-item.nav-link,
+    nav>div a.nav-item.nav-link.active {
+        border: none;
+        padding: 18px 25px;
+        color: #fff;
+        background: #2d7a9e;
+        border-radius: 0;
+        font-size: 24px;
+        line-height: 34px;
+        font-weight: 600;
+        font-family: Poppins;
+    }
 
-border: none;
-  color:#fff;
-  background:#272e38;
-  border-radius:0;
+    nav>div a.nav-item.nav-link.active:after {
+        content: "";
+        position: relative;
+        bottom: -70px;
+        left: -20%;
+        border: 15px solid transparent;
+        border-top-color: #6ec1e6;
+    }
 
-}
-nav > div a.nav-item.nav-link,
-nav > div a.nav-item.nav-link.active
-{
-border: none;
-  padding: 18px 25px;
-  color:#fff;
-  background:#2d7a9e;
-  border-radius:0;
-}
+    .tab-content {
+        background: #f5fbfd;
+        line-height: 25px;
+        border: 1px solid #ddd;
+        border-top: 5px solid #6ec1e6;
+        border-bottom: none;
+        padding: 50px 25px 100px;
+    }
 
-nav > div a.nav-item.nav-link.active:after
-{
-content: "";
-position: relative;
-bottom: -60px;
-left: -10%;
-border: 15px solid transparent;
-border-top-color: #6ec1e6 ;
-}
-.tab-content{
-background: #f5fbfd;
-  line-height: 25px;
-  border: 1px solid #ddd;
-  border-top:5px solid #6ec1e6;
-  border-bottom:none;
-  padding:30px 25px 100px;
-}
+    nav>div a.nav-item.nav-link:hover,
+    nav>div a.nav-item.nav-link:focus {
+        border: none;
+        background: #6ec1e6;
+        color: #fff;
+        border-radius: 0;
+        transition: background 0.20s linear;
+    }
 
-nav > div a.nav-item.nav-link:hover,
-nav > div a.nav-item.nav-link:focus
-{
-border: none;
-  background: #6ec1e6;
-  color:#fff;
-  border-radius:0;
-  transition:background 0.20s linear;
-}
 </style>
 
 <!-- Main content Start -->
@@ -56,7 +57,7 @@ border: none;
     <div class="rs-breadcrumbs bg-4">
         <div class="container">
             <div class="content-part text-center">
-                <h1 class="breadcrumbs-title white-color mb-0">nShield Connect HSMs</h1>
+                <h1 class="breadcrumbs-title white-color mb-0">nShield Connect HSMs 1</h1>
             </div>
         </div>
     </div>
@@ -70,8 +71,13 @@ border: none;
                     <div class="sec-title3">
                         <!-- <span class="sub-title">~ <span class="title-upper">About Us</span> ~</span> -->
                         <h2 class="title title2 pb-25">nShield Connect HSMs</h2>
-                        <p class="description text-justify pb-10">nShield Connect HSMs are certified hardware security appliances that deliver cryptographic services to a variety of applications across the network. nShield HSM appliances are hardened, tamper-resistant platforms that perform such functions as encryption, digital signing, and key generation and protection.</p>
-                        <p class="description text-justify pb-30">With their comprehensive capabilities, these HSMs can support an extensive range of applications, including certificate authorities, code signing and more.</p>
+                        <p class="description text-justify pb-10">nShield Connect HSMs are certified hardware security
+                            appliances that deliver cryptographic services to a variety of applications across the
+                            network. nShield HSM appliances are hardened, tamper-resistant platforms that perform such
+                            functions as encryption, digital signing, and key generation and protection.</p>
+                        <p class="description text-justify pb-30">With their comprehensive capabilities, these HSMs can
+                            support an extensive range of applications, including certificate authorities, code signing
+                            and more.</p>
                         <ul class="btn-part">
                             <li><a class="readon2 get-new" href="#">Download Datasheet</a></li>
                         </ul>
@@ -84,7 +90,7 @@ border: none;
                 </div>
             </div>
         </div>
-        
+
     </div>
     <!-- About Section End -->
 
@@ -94,15 +100,23 @@ border: none;
             <div class="sec-title4 text-center mb-50">
                 <!-- <span class="sub-title new pb-10">MANAGED IT SERVICES</span> -->
                 <h2 class="title title3">Remote configuration eliminates costly trips to the data center</h2>
-                <p class="text-justify mt-4">The latest nShield Connect XC models offer an optional serial port that allows enterprises to eliminate costly repeat trips to the data center. Remote Configuration capabilities include:</p>
+                <p class="text-justify mt-4">The latest nShield Connect XC models offer an optional serial port that
+                    allows enterprises to eliminate costly repeat trips to the data center. Remote Configuration
+                    capabilities include:</p>
 
                 <ul class="listing-style text-left regular2 pl-10 sm-pl-0">
                     <li>Initiating and changing an HSM’s network settings, e.g. IP address</li>
-                    <li>Supporting provider/tenant deployment models where the nShield HSM appliance can be easily configured by the provider before passing control of the HSM to a tenant. Separation of roles ensures the cryptographic key material is not exposed to the provider.</li>
-                    <li>Purging key material and decommissioning the nShield HSM appliance at the end of a usage cycle in preparation for its next deployment.</li>
-                </ul>                
+                    <li>Supporting provider/tenant deployment models where the nShield HSM appliance can be easily
+                        configured by the provider before passing control of the HSM to a tenant. Separation of roles
+                        ensures the cryptographic key material is not exposed to the provider.</li>
+                    <li>Purging key material and decommissioning the nShield HSM appliance at the end of a usage cycle
+                        in preparation for its next deployment.</li>
+                </ul>
 
-                <p class="text-justify mt-4">Technicians simply need to rack and cable the nShield HSM appliance and connect a serial concentrator in the data center to prepare the nShield Connect XC for full remote configuration and administration. This reduces the need for trained resources in the data center and provides customers more efficiency and control over their HSMs.</p>
+                <p class="text-justify mt-4">Technicians simply need to rack and cable the nShield HSM appliance and
+                    connect a serial concentrator in the data center to prepare the nShield Connect XC for full remote
+                    configuration and administration. This reduces the need for trained resources in the data center and
+                    provides customers more efficiency and control over their HSMs.</p>
             </div>
         </div>
     </div>
@@ -114,16 +128,19 @@ border: none;
                     <div class="services-item">
                         <div class="services-wrap">
                             <div class="shape-part">
-                                <img class="up-down-new" src="/assets/images/services/apps/1.png" alt="images">             
+                                <img class="up-down-new" src="/assets/images/services/apps/1.png" alt="images">
                             </div>
                             <div class="icon-part">
                                 <i class="fa fa-bar-chart-o"></i>
                             </div>
                             <div class="services-content">
-                                <div class="services-title"><h3 class="title"><a href="services-single.html">Powerful Architecture</a></h3></div>
-                                <p class="services-txt">Build and grow your HSM estate using Security World, Entrust's unified ecosystem that delivers scalability, seamless failover, and load balancing.
+                                <div class="services-title">
+                                    <h3 class="title"><a href="services-single.html">Powerful Architecture</a></h3>
+                                </div>
+                                <p class="services-txt">Build and grow your HSM estate using Security World, Entrust's
+                                    unified ecosystem that delivers scalability, seamless failover, and load balancing.
                                 </p>
-                            </div>          
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -131,14 +148,17 @@ border: none;
                     <div class="services-item">
                         <div class="services-wrap">
                             <div class="shape-part">
-                                <img class="up-down-new" src="/assets/images/services/apps/2.png" alt="images">             
+                                <img class="up-down-new" src="/assets/images/services/apps/2.png" alt="images">
                             </div>
                             <div class="icon-part purple-bg">
                                 <i class="fa fa-clone"></i>
                             </div>
                             <div class="services-content">
-                                <div class="services-title"><h3 class="title"><a href="services-single.html">Faster Data Processing</a></h3></div>
-                                <p class="services-txt">Get some of the highest cryptographic transaction rates in the industry. Ideal for environments where throughput is critical.</p>
+                                <div class="services-title">
+                                    <h3 class="title"><a href="services-single.html">Faster Data Processing</a></h3>
+                                </div>
+                                <p class="services-txt">Get some of the highest cryptographic transaction rates in the
+                                    industry. Ideal for environments where throughput is critical.</p>
                             </div>
                         </div>
                     </div>
@@ -147,15 +167,19 @@ border: none;
                     <div class="services-item">
                         <div class="services-wrap">
                             <div class="shape-part">
-                                <img class="up-down-new" src="/assets/images/services/apps/3.png" alt="images">             
-                                </div>
-                                <div class="icon-part blue-bg">
+                                <img class="up-down-new" src="/assets/images/services/apps/3.png" alt="images">
+                            </div>
+                            <div class="icon-part blue-bg">
                                 <i class="fa fa-sitemap"></i>
-                                </div>
+                            </div>
                             <div class="services-content">
-                                <div class="services-title"><h3 class="title"><a href="services-single.html">Protection of sensitive business and application logic</a></h3></div>
-                                <p class="services-txt">Execute code within nShield boundaries, protecting your applications and the data they process.</p>
-                            </div>  
+                                <div class="services-title">
+                                    <h3 class="title"><a href="services-single.html">Protection of sensitive business
+                                            and application logic</a></h3>
+                                </div>
+                                <p class="services-txt">Execute code within nShield boundaries, protecting your
+                                    applications and the data they process.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -167,31 +191,50 @@ border: none;
         <div class="col-xs-12">
             <nav>
                 <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                    <a class="nav-item nav-link active title" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Tech Specs</a>
-                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Options and Accessories</a>
-                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Related Resources</a>
+                    <a class="nav-item nav-link active title" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+                        role="tab" aria-controls="nav-home" aria-selected="true">Tech Specs</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
+                        aria-controls="nav-profile" aria-selected="false">Options and Accessories</a>
+                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
+                        aria-controls="nav-contact" aria-selected="false">Related Resources</a>
                 </div>
             </nav>
-        
+
             <div class="tab-content pb-100 px-3 px-sm-0" id="nav-tabContent">
-            
-                <div class="tab-pane fade show active container" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+
+                <div class="tab-pane fade show active container" id="nav-home" role="tabpanel"
+                    aria-labelledby="nav-home-tab">
+                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat
+                    veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim.
+                    Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim
+                    non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor
+                    ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                    Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
                 </div>
                 <div class="tab-pane fade container" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat
+                    veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim.
+                    Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim
+                    non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor
+                    ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                    Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
                 </div>
                 <div class="tab-pane fade container" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
-                </div>            
-            </div>            
-        
-        </div>
-        
-    </div>
-    
+                    Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat
+                    veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim.
+                    Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim
+                    non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor
+                    ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore.
+                    Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                </div>
+            </div>
 
-    
-</div> 
+        </div>
+
+    </div>
+
+
+
+</div>
 <!-- Main content End -->
 @endsection
